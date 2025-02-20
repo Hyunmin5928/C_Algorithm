6b084f12a7ff2dcb612e111dcaac43e62f76cda7
hyunmin <hunmin5928@gmail.com>
2025-02-20 16:58:08 +0900

    SLL

---
+++ LinkedList.h
@@ -0,0 +1,27 @@
+﻿#ifndef LINKEDLIST_H
+#define LINKEDLIST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+
+typedef struct s_Node {
+	ElementType data;
+	struct s_Node* NextNode;
+	// Node* NextNode;
+} Node;
+
+Node* SLL_CreateNode(ElementType Newdata);
+void SLL_DestroyNode(Node* Node);
+void SLL_AppendNode(Node** Head, Node* NewNode);
+void SLL_InsertAfter(Node* Current, Node* NewNode);
+void SLL_InsertNewHead(Node** Head, Node* NewNode);
+void SLL_RemoveNode(Node** Head, Node* RemoveNode);
+Node* SLL_GetNodeAt(Node* Head, int Location);
+int SLL_GetNodeCount(Node* Head);
+
+void SLL_InsertBefore(Node** Head, Node* Current, Node* NewHead);
+void SLL_DestroyAllNodes(Node** List);
+
+#endif
---
+++ Test.c
@@ -0,0 +1,61 @@
+﻿#include "LinkedList.h"
+
+int main(void) {
+	int count = 0;
+	Node* List = NULL;
+	Node* Current = NULL;
+	Node* NewNode = NULL;
+
+	for (int i = 0; i < 5; i++) {
+		NewNode = SLL_CreateNode(i * 4);
+		SLL_AppendNode(&List, NewNode);
+	}
+	NewNode = SLL_CreateNode(-1);
+	SLL_AppendNode(&List, NewNode);
+
+	NewNode = SLL_CreateNode(-2);
+	SLL_AppendNode(&List, NewNode);
+
+	// List 출력
+	count = SLL_GetNodeCount(List);
+	for (int i = 0; i < count; i++) {
+		Current = SLL_GetNodeAt(List, i);
+		printf("List[%d] = %d\n", i, Current->data);
+	}
+
+	printf("\nInserting 3000 After index [2]...\n\n");
+
+	Current = SLL_GetNodeAt(List, 2);
+	NewNode = SLL_CreateNode(2000);
+	SLL_InsertAfter(Current, NewNode);
+
+	Current = SLL_GetNodeAt(List, 1);
+	NewNode = SLL_CreateNode(4000);
+	SLL_InsertBefore(&List, Current, NewNode);
+
+	// List 출력
+	count = SLL_GetNodeCount(List);
+	for (int i = 0; i < count; i++) {
+		Current = SLL_GetNodeAt(List, i);
+		printf("List[%d] = %d\n", i, Current->data);
+	}
+
+	printf("\nDestroying List\n");
+	//for (int i = 0; i < count; i++) {
+	//	Current = SLL_GetNodeAt(List, 0);
+	//	if (Current != NULL) {
+	//		printf("Destroy Data of List = %d\n", Current->data);
+	//		SLL_RemoveNode(&List, Current);
+	//		SLL_DestroyNode(Current);
+	//	}
+	//}
+
+	SLL_DestroyAllNodes(&List);
+	count = SLL_GetNodeCount(List);
+	for (int i = 0; i < count; i++) {
+		Current = SLL_GetNodeAt(List, i);
+		printf("List[%d] = %d\n", i, Current->data);
+	}
+
+	return 0;
+}
