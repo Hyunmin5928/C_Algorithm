e2b6680ca61b94fe8c8a004f8142c8a2cd1a1042
hyunmin <hunmin5928@gmail.com>
2025-02-21 11:52:51 +0900

    array stack 생성

--- ArrayStack.c
+++ ArrayStack.c
@@ -1,1 +1,3 @@
-#pragma once
+﻿#include "ArrayStack.h"
+
+// 스택 및 노드 생성/소멸 연산
--- ArrayStack.h
+++ ArrayStack.h
@@ -4,4 +4,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int ElementType;
+
+// 스택의 각 층을 구성하는 노드 구조체
+typedef struct s_AR_Node {
+	ElementType data;
+} AR_Node;
+
+// 스택 구조체
+typedef struct s_ArrayStack {
+	int Capacity;
+	int top;
+	AR_Node* Nodes;
+} ArrayStack;
+
+void AS_CreateStack(ArrayStack** Stack, int Capacity) {
+
+}
+
 #endif
