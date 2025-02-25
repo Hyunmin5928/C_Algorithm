24fdb057e98cf9a13ed03c7cdf0d549d8a25c2b3
hyunmin <hunmin5928@gmail.com>
2025-02-25 10:44:16 +0900

    0

---
+++ CircularQueue.c
@@ -0,0 +1,22 @@
+﻿#include "CircularQueue.h"
+
+// 순환 큐 생성 소멸 연산
+void CQ_CreateQueue(CircularQueue** Queue, int Capacity) {
+	(*Queue) = (CircularQueue*)malloc(sizeof(CircularQueue));
+
+	(*Queue)->Nodes = (CQ_Node*)malloc(sizeof(CQ_Node) * (Capacity + 1));
+
+	(*Queue)->Capacity = Capacity;
+	(*Queue)->Front = 0;
+	(*Queue)->Rear = 0;
+}
+
+void CQ_DestroyQueue(CircularQueue* Queue) {
+	free(Queue->Nodes);
+	free(Queue);
+}
+
+// 노드 삽입 연산
+void CQ_Enqueue(CircularQueue* Queue, ElementType data) {
+
+}
---
+++ CircularQueue.h
@@ -0,0 +1,21 @@
+﻿#ifndef CIRCULAR_QUEUE_H
+#define CIRCULAR_QUEUE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int ElementType;
+
+typedef struct s_CQ_Node {
+	ElementType data;
+}CQ_Node;
+
+typedef struct s_CircularQueue {
+	int Capacity;
+	int Front;
+	int Rear;
+
+	CQ_Node* Nodes;
+}CircularQueue;
+
+#endif
