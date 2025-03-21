775a706f83a030cfdfb2a27af514a259f86b4071
Jang hyunmin <hunmin5928@gmail.com>
2025-03-21 11:31:37 +0900

    연산자 함수

---
+++ C++/2025-03-21/OperatorFunc/example_1.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+class Time
+{
+private:
+    int hour, min, sec;
+public:
+    Time() { }
+    Time(int h, int m, int s){
+        hour = h; min = m; sec = s;
+    }
+    void SetTime(int h, int m, int s) {
+        hour = h; min = m; sec = s;
+    }
+    void OutTime() const {
+        printf("현재 시간은 %d:%d:%d입니다.\n", hour, min, sec);
+    }
+    const Time operator +(const Time &other) const {
+        Time t;
+        t.sec = sec + other.sec;
+        t.min = min + other.min;
+        t.hour = hour + other.hour;
+        
+        t.min += t.sec / 60;
+        t.sec %= 60;
+        t.hour += t.min / 60;
+        t.min %= 60;
+        t.hour %= 24;
+    
+        return t;
+    }
+};
+
+int main(void){
+    Time t1(15, 10, 30);
+    Time t2(23, 20, 40);
+    Time t3;    // 기본 연산자를 필요로 하는 부분
+
+    t3 = t1 + t2;
+
+    t3.OutTime();   // 현재 시간은 14:31:10입니다.
+
+    return 0;
+}
