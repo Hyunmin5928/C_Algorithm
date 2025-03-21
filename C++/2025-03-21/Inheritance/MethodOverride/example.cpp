bc25c7aac5456a9aba663a27acb98b285b660e36
Jang hyunmin <hunmin5928@gmail.com>
2025-03-21 14:12:18 +0900

    상속-권한 상속-멤버 함수 오버라이딩

--- C++/2025-03-21/Inheritance/MethodOverride/example.cpp
+++ C++/2025-03-21/Inheritance/MethodOverride/example.cpp
@@ -2,7 +2,7 @@
 #include <string.h>
 
 class Human{
-private: 
+protected:           // 자식에게 허용
     char name[12];
     int age;
 public:
@@ -16,14 +16,20 @@ public:
 };
 
 class Student : public Human{
-private:
+protected:
     int stunum;
 public:
     Student(const char *aname, int aage, int astunum) : Human(aname, aage) {
         stunum = astunum;
     }
     void study() {
-        printf("학번 %d 학생이 공부하고 있습니다.\n", stunum);
+        printf("%s 학생 학습 중.\n", name);
+    }
+    void report() {
+        printf("이름 : %s, 학번 : %d 보고서 제출.\n", name, stunum);
+    }
+    void intro(){       // method overriding
+        printf("%d학번 %s입니다.\n", stunum, name);
     }
 };
 
@@ -31,8 +37,9 @@ int main(){
     Human kim("김상형", 29);
     kim.intro();                            // 이름 = 김상형, 나이 = 29
     Student han("김한결", 15, 123456);
-    han.intro();                            // 이름 = 김한결, 나이 = 15
-    han.study();                            // 학번 123456 학생이 공부하고 있습니다.
+    han.intro();                            // 123456학번 김한결입니다.
+    han.study();                            // 김한결 학생 학습 중.
+    han.report();                           // 이름 : 김한결, 학번 : 123456 보고서 제출.
 
     return 0;
 }
